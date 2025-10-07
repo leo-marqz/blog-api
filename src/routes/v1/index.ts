9b26400b40848aaffcfeae7e630eaf00f8db3ba1
Elmer Marquez <leomarqz2020@gmail.com>
2025-10-07 16:58:23 -0600

    feat: the router was added

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -4,5 +4,21 @@
  * @license Apache-2.0
  */
 
+// Node Modules
+import { Router } from 'express';
 
+const router = Router();
+
+//Router
+router.get('', (req, res)=>{
+    res.json({
+        message: 'API is live',
+        status: 'ok',
+        version: '1.0.0',
+        docs: 'https://docs.blog-api.leomarqz.com',
+        timestamp: new Date().toISOString()
+    })
+})
+
+export default router;
 
--- src/server.ts
+++ src/server.ts
@@ -4,24 +4,39 @@
  * @license Apache-2.0
  */
 
-// Node Modules
+/**
+ * Node Modules
+ */
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import helmet from 'helmet';
 
-// Custom Modules
+/**
+ * Custom Modules
+ */ 
 import config from './config';
 import limiter from './lib/express_rate_limit';
 
-// Types
+/**
+ * Router
+ */ 
+import v1Router from './routes/v1';
+
+/**
+ * Types
+ */ 
 import type { CorsOptions } from 'cors';
 
-// Express app initial
+/**
+ * Express app initial
+ */ 
 const app = express();
 
-//Configure CORS options
+/**
+ * Configure CORS options
+ */
 const corsOptions: CorsOptions = {
     origin: (origin, callback)=> {
         if( config.NODE_ENV === 'development' 
@@ -38,46 +53,66 @@ const corsOptions: CorsOptions = {
     }
 } 
 
-// Apply CORS Middleware
+/**
+ * Apply CORS Middleware
+ */
 app.use( cors( corsOptions ) );
 
-// Enable JSON request body parsing
+/**
+ * Enable JSON request body parsing
+ */
 app.use( express.json() ); 
 
-// Enable URL-Encodeed request body parsing with extended mode
-// `extended: true` allows rich objects and arrays via querystring library
+/**
+ * Enable URL-Encodeed request body parsing with extended mode
+ * `extended: true` allows rich objects and arrays via querystring library
+ */
 app.use( express.urlencoded({ extended: true }) );
 
+/**
+ * Enable Cookie Parser
+ */
 app.use( cookieParser() );
 
-// Enable response compression to reduce payload size improve performance
+/**
+ * Enable response compression to reduce payload size improve performance
+ */
 app.use( 
     compression({
         threshold: 1024, // Only compress responses larger than 1KB
     }) 
 );
-
-// Use Helmet to enhance security by setting various HTTP headers
+/**
+ * Use Helmet to enhance security by setting various HTTP headers
+ */
 app.use( helmet() );
 
-// Apply rate limiting middleware to prevent excessive requests and enhance security
+/**
+ * Apply rate limiting middleware to prevent excessive requests and enhance security
+ */
 app.use( limiter );
 
+/**
+ * Apply main router - v1
+ */
+app.use('/api/v1', v1Router);
+// app.use('/api/v2', v2Router);
+
+/**
+ * Immediately Invoked Async Function Expression (IIFE) to start the server.
+ * 
+ * - Tries to connect to the datase before initializing the server.
+ * - Defines the API router ('/api/v1').
+ * - Starts the server on the specified  PORT and logs the running URL.
+ * - If an error occurs during startup, it is logged, and the process exits with status 1.
+ */
 (async ()=>{
     try{
-        //Router
-        app.get('', (req, res)=>{
-            res.json({
-                id: 1,
-                name: 'leomarqz',
-                nickname: '::crack::night::'
-            })
-        })
-        
-        // Express server listening...
+
         app.listen(config.PORT, ()=>{
             console.log(`Server is running: http://localhost:${config.PORT}`);
         });
+        
     }catch(error){
         console.log(`failed to start the server`, error);
 
